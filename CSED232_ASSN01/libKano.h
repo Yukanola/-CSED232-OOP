3312908d6d00fb2f48eb7f86093cabc28acc2c45
Yukanola <iknowme@postech.edu>
2016-10-02 16:58:31 +0900

    libKano update

--- CSED232_ASSN01/libKano.h
+++ CSED232_ASSN01/libKano.h
@@ -91,25 +91,7 @@ namespace _Kano {
 
 	public:
 
-		__con() {
-
-			if (isWin) {
-
-				CONSOLE_SCREEN_BUFFER_INFO csBuffer;
-				GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csBuffer);
-
-				this->consolSize[0] = csBuffer.srWindow.Bottom - csBuffer.srWindow.Top + 1;
-				this->consolSize[1] = csBuffer.srWindow.Right - csBuffer.srWindow.Left + 1;
-
-			}
-			else {
-
-				this->consolSize[0] = std::system("echo $LINES");
-				this->consolSize[1] = std::system("echo $COLUMNS");
-
-			}
-
-		};
+		inline __con() {};
 
 		void setCS(int cols, int lines) {
 
@@ -362,7 +344,31 @@ namespace _Kano {
 
 		};
 
-	} con;
+	};
+
+	#ifdef isWin
+
+	__con::__con {
+
+		CONSOLE_SCREEN_BUFFER_INFO csBuffer;
+		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csBuffer);
+
+		this->consolSize[0] = csBuffer.srWindow.Bottom - csBuffer.srWindow.Top + 1;
+		this->consolSize[1] = csBuffer.srWindow.Right - csBuffer.srWindow.Left + 1;
+
+	}
+
+	#elif isLinux
+
+	__con::__con {
+
+		this->consolSize[0] = std::system("echo $LINES");
+		this->consolSize[1] = std::system("echo $COLUMNS");
+
+	}
+	#endif
+
+	__con con;
 
 	static class __core {
 
